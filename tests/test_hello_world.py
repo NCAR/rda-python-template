546c32f5d88d15997776a84741d61ae5c3ff0e84
Zaihua Ji <zji@ucar.edu>
2025-01-15 11:27:49 -0600

    Create test_hello_world.py

---
+++ tests/test_hello_world.py
@@ -0,0 +1,11 @@
+# test_hello_world.py
+
+import pytest
+from rda_python_template.hello_world import get_string
+
+def test_get_string():
+    assert get_string('Bob') == 'Bob: Hello World!'
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        get_string(9)
