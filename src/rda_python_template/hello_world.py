c18d9df76aa6770a355b862032aace81bca0ae49
Zaihua Ji <zji@ucar.edu>
2025-01-15 11:14:00 -0600

    Create hello_world.py

---
+++ src/rda_python_template/hello_world.py
@@ -0,0 +1,13 @@
+#
+# main function to run hello_world
+#
+def main():
+
+   str = "Hello World!"
+   print(str)
+   return str
+
+#
+# call main() to start program
+#
+if __name__ == "__main__": main()
